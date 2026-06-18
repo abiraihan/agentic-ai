c98099aad3f6a68fca887759b131f69929a6b3a1
abiraihan <abirraihan.urp@gmail.com>
2026-06-18 13:12:38 -0400

    Added main update

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from typing import List
+
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
+
 load_dotenv()
 from langchain.agents import create_agent
 from langchain.tools import tool
@@ -8,16 +12,38 @@ from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
 
+class Source(BaseModel):
+    """Scheme for a source used by the agent"""
+
+    url: str = Field(description="The URL of the source")
+
+
+class AgentResponse(BaseModel):
+    """Scheme for the Agent Response with Answer and Sources"""
+
+    answer: str = Field(description="The agent's answer to the query")
+    sources: List[Source] = Field(
+        default_factory=list, description="List of sources used to generate teh answer"
+    )
+
+
 llm = ChatOpenAI(model="gpt-5")
 
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
+
+
 def main():
     print("Hello from agentic-ai!")
-    result = agent.invoke({"messages": HumanMessage(content="Search for 3 job posting for an ai engineer using langchain in raleigh area on linkedin and list their details")})
+    result = agent.invoke(
+        {
+            "messages": HumanMessage(
+                content="Search for 3 job posting for an ai engineer using langchain in raleigh area on linkedin and list their details"
+            )
+        }
+    )
     print(result)
 
 
 if __name__ == "__main__":
     main()
-    
